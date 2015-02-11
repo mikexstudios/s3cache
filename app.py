385b0721ea5c75c2e03e0a897799ed90d7853c0a
Michael Huynh <mike.huynh@gmail.com>
2015-02-10 22:55:55 -0500

    Implemented catch all that prints back out path.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+
+import urllib
+
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
-def hello():
+def home():
     return 'Hello World!'
+
+@app.route('/<path:key>')
+def cache(key):
+    # reconstruct original S3 path:
+    query_str = urllib.urlencode(request.args.lists(), doseq=True)
+    full_path = '%s?%s' % (key, query_str)
+
+    return '%s' % full_path

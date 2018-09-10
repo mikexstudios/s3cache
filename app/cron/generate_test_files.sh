c69d54e7f8413d1b2c0335795beca04e5aa9bbd1
Michael Huynh <mike.huynh@gmail.com>
2018-09-09 20:11:20 -0400

    Move cron into app/. Update requirements.txt

--- app/cron/generate_test_files.sh
+++ app/cron/generate_test_files.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 for i in {1..1000}
 do
-    RANDOM_MINUTE=$(python -S -c "import random; print '%02i' % random.randrange(0,59)")
+  RANDOM_MINUTE=$(python -S -c "import random; print('%02i' % random.randrange(0,59))")
     touch -t011119$RANDOM_MINUTE $i
     echo "Creating $i with minute $RANDOM_MINUTE..."
 done
